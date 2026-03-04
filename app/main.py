8e76fd281b4b1840b5cf224a4be025dfe086b24a
JuanPabloCB <contacto1juanp@gmail.com>
2026-03-04 03:03:31 -0500

    feature: change in main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.api.router import api_router
 from app.core.config import settings
+from fastapi.middleware.cors import CORSMiddleware
+from app.api.v1.router import router as api_v1_router
 
 app = FastAPI(
     title=settings.APP_NAME,
@@ -8,6 +10,16 @@ app = FastAPI(
 )
 
 app.include_router(api_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(api_v1_router, prefix="/api/v1")
 
 @app.get("/")
 def root():
